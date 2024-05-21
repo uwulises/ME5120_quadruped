8401953963b86257873ee48ac715602264e3cee2
uwulises <ulises.campodonico@ug.uchile.cl>
2024-05-20 22:05:53 -0400

    update

--- model/test.py
+++ model/test.py
@@ -7,22 +7,14 @@ data = mujoco.MjData(model)
 model.opt.gravity = (0, 0, -9.81)
 
 with mujoco.viewer.launch(model, data, show_left_ui=True,show_right_ui=True) as viewer:
-  # Close the viewer automatically after 30 wall-seconds.
   start = time.time()
   while viewer.is_running():
     step_start = time.time()
-
     # mj_step can be replaced with code that also evaluates
     # a policy and applies a control signal before stepping the physics.
     mujoco.mj_step(model, data)
-
-    # Example modification of a viewer option: toggle contact points every two seconds.
-    with viewer.lock():
-      viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = int(data.time % 2)
-
     # Pick up changes to the physics state, apply perturbations, update options from GUI.
     viewer.sync()
-
     # Rudimentary time keeping, will drift relative to wall clock.
     time_until_next_step = model.opt.timestep - (time.time() - step_start)
     if time_until_next_step > 0:

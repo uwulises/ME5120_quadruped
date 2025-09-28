78a5f265011868879650d4680c48d7e874037c38
uwulises <ulises.campodonico@ug.uchile.cl>
2025-09-28 00:13:14 -0300

    add test files for leg control

---
+++ control/leg_control/test_serial.py
@@ -0,0 +1,18 @@
+from serial_control import SerialControl
+import time
+# Create an instance of the SerialControl class
+motor = SerialControl(port="COM17", baudrate=115200)
+
+#create a list of radians to send to the motor
+radians = [0, 1, 2, 0, 4, 5, 0]
+
+# Open the serial port
+motor.open_serial()
+
+# Send the commands to the motor
+for rad in radians:
+    motor.send_command([rad, rad, rad])
+    time.sleep(0.5)
+
+# Close the serial port
+motor.close_serial()
